b126a333c6a054d803d10983f5cc03b753c3b679
Aditya Kumar <89588001+Aditya7j@users.noreply.github.com>
2026-03-16 21:10:09 +0530

    Food App

---
+++ App.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import ReactDOM from "react-dom/client";
+import { MainComponent, Navbar } from "./src/components/Navbar";
+import { Footer } from "./src/components/Footer";
+
+const AppLayout = () => {
+
+    const [list, setList] = useState([]);
+    const [loader, setLoader] = useState(false);
+    const [error, setError] = useState('');
+
+    return (
+        <>
+            <Navbar list={list} setList={setList}
+                loader={loader} setLoader={setLoader}
+                error={error} setError={setError} />
+            <MainComponent list={list} loader={loader} setLoader={setLoader}
+                error={error} setError={setError} />
+            <Footer />
+        </>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
---
+++ data.js
@@ -0,0 +1,38 @@
+export const data = [
+    {
+        name: "Burger King",
+        img: "https://www.shutterstock.com/image-illustration/beef-hamburger-cheese-mockup-on-600nw-2580579617.jpg",
+        rating: "4.2",
+        cusines: ["Burgers", "American"],
+    },
+    {
+        name: "KFC",
+        img: "https://t4.ftcdn.net/jpg/05/51/17/79/360_F_551177904_MSQRgTsYvKJX37OrpnNwTxSbVfSxk8Yd.jpg",
+        rating: "4.6",
+        cusines: ["KFC", "American", "Indians"],
+    },
+    {
+        name: "Food Adda",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwCH84BwbczApmpxSfWKV_X32DndZkdhSCjA&s",
+        rating: "4.0",
+        cusines: ["Indians", "American", "Italian"],
+    },
+    {
+        name: "Somasa Adda",
+        img: "https://c.ndtvimg.com/2022-09/lpcnb0g8_samosa_625x300_29_September_22.jpg",
+        rating: "4. 7",
+        cusines: ["Somonsa", "Indian"],
+    },
+    {
+        name: "Delhi Food",
+        img: "https://assets.cntraveller.in/photos/623959d9af0baef6cf18daef/master/w_1600%2Cc_limit/636144902",
+        rating: "4.5",
+        cusines: ["Delhi", "Indian", "Bihar"],
+    },
+    {
+        name: "Uncle Ji Food",
+        img: "https://b.zmtcdn.com/data/pictures/7/19904777/02523e84541797f10d7192cc48894746.jpeg?fit=around|960:500&crop=960:500;*,*",
+        rating: "3.8",
+        cusines: ["Delhi", "Indian", "Bihar", "Marathi"],
+    }
+]
